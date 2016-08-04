3e42e0d7f3681773a229da1c32d811c15360597d
migueltet <macabreras@unl.edu.ec>
2016-08-04 09:47:23 -0500

    Proyecto Login en Android con SQLlite
    Hola...
    Saludos

---
+++ app/src/main/java/com/example/raul/login/Menu.java
@@ -0,0 +1,17 @@
+package com.example.raul.login;
+
+/**
+ * Created by Miguel on 3/8/2016.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+}
